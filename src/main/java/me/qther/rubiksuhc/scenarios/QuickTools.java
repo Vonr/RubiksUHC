6b9725ab3cd2070a14d414890d603cc148ab0ee8
Qther <dtaclanlegion@gmail.com>
2020-05-11 17:00:30 +0800

    fix quick tools

--- src/main/java/me/qther/rubiksuhc/scenarios/QuickTools.java
+++ src/main/java/me/qther/rubiksuhc/scenarios/QuickTools.java
@@ -14,18 +14,19 @@ public class QuickTools implements Listener {
     
     @EventHandler
     public void onCraftItem(CraftItemEvent event) {
-        ItemStack result = event.getRecipe().getResult();
-        Material material = result.getType();
-        if (material == Material.WOODEN_PICKAXE || material == Material.STONE_PICKAXE || material == Material.GOLDEN_PICKAXE || material == Material.IRON_PICKAXE || material == Material.DIAMOND_PICKAXE ||
-                material == Material.WOODEN_AXE || material == Material.STONE_AXE || material == Material.GOLDEN_AXE || material == Material.IRON_AXE || material == Material.DIAMOND_AXE ||
-                material == Material.WOODEN_SHOVEL || material == Material.STONE_SHOVEL || material == Material.GOLDEN_SHOVEL || material == Material.IRON_SHOVEL || material == Material.DIAMOND_SHOVEL) {
-            HumanEntity crafter = event.getWhoClicked();
-            ItemMeta itemMeta = result.getItemMeta();
-            itemMeta.addEnchant(Enchantment.DIG_SPEED, 3, false);
-            itemMeta.addEnchant(Enchantment.DURABILITY, 1, false);
-            result.setItemMeta(itemMeta);
-            event.setCurrentItem(result);
+        if (enabled) {
+            ItemStack result = event.getRecipe().getResult();
+            Material material = result.getType();
+            if (material == Material.WOODEN_PICKAXE || material == Material.STONE_PICKAXE || material == Material.GOLDEN_PICKAXE || material == Material.IRON_PICKAXE || material == Material.DIAMOND_PICKAXE ||
+                    material == Material.WOODEN_AXE || material == Material.STONE_AXE || material == Material.GOLDEN_AXE || material == Material.IRON_AXE || material == Material.DIAMOND_AXE ||
+                    material == Material.WOODEN_SHOVEL || material == Material.STONE_SHOVEL || material == Material.GOLDEN_SHOVEL || material == Material.IRON_SHOVEL || material == Material.DIAMOND_SHOVEL) {
+                HumanEntity crafter = event.getWhoClicked();
+                ItemMeta itemMeta = result.getItemMeta();
+                itemMeta.addEnchant(Enchantment.DIG_SPEED, 3, false);
+                itemMeta.addEnchant(Enchantment.DURABILITY, 1, false);
+                result.setItemMeta(itemMeta);
+                event.setCurrentItem(result);
+            }
         }
-
     }
 }
